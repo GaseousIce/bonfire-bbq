968b01a53d9a664cf5cf9cca761a69edebec1728
GaseousIce <104217734+GaseousIce@users.noreply.github.com>
2026-06-24 21:42:38 +0530

    perf: optimize images with Astro assets

---
+++ src/assets/images.ts
@@ -0,0 +1,31 @@
+import cardamomMalaiKebab from './images/cardamom_malai_kebab.png';
+import chickenDumBiryani from './images/chicken_dum_biryani.png';
+import chickenJungleeKebab from './images/chicken_junglee_kebab.png';
+import classicHandiBiryani from './images/classic_handi_biryani.png';
+import crispyFriedMomos from './images/crispy_fried_momos.png';
+import galleryBiryani from './images/gallery_biryani.png';
+import gallerySigree from './images/gallery_sigree.png';
+import gallerySpices from './images/gallery_spices.png';
+import galleryTandoor from './images/gallery_tandoor.png';
+import hearthRoastedShawarma from './images/hearth_roasted_shawarma.png';
+import heroTandoor from './images/hero_tandoor.png';
+import stuffedMozzarellaChicken from './images/stuffed_mozzarella_chicken.png';
+import tandooriChicken from './images/tandoori_chicken.png';
+import tandooriMiniPlatter from './images/tandoori_mini_platter.png';
+
+export {
+  cardamomMalaiKebab,
+  chickenDumBiryani,
+  chickenJungleeKebab,
+  classicHandiBiryani,
+  crispyFriedMomos,
+  galleryBiryani,
+  gallerySigree,
+  gallerySpices,
+  galleryTandoor,
+  hearthRoastedShawarma,
+  heroTandoor,
+  stuffedMozzarellaChicken,
+  tandooriChicken,
+  tandooriMiniPlatter,
+};
